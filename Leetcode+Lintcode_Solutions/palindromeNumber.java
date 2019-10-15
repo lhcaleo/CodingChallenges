0dd716f6059bfb391a2f77ba0325f7b5658cf4ca
hao <lihaocaleo@gmail.com>
2019-10-14 23:28:10 -0400

    solved Palindrome Number

---
+++ Leetcode+Lintcode_Solutions/palindromeNumber.java
@@ -0,0 +1,17 @@
+class Solution {
+	public boolean isPalindrome(int x) {
+		// negative numbers are not palindrome
+		if(x<0) return false;
+		if(x==0) return true;
+		int copy = x;
+		int rev = 0;
+		// reverse the integer
+		while(copy!=0)
+		{
+			int digit = copy%10;
+			rev = rev * 10 + digit;
+			copy /= 10;
+		}
+		return rev == x;
+	}
+}
