f1f80a3d74267e0260f55d00c57deb5385135807
hao <lihaocaleo@gmail.com>
2020-01-04 22:27:24 -0500

    update Recursion solution

--- Leetcode_Solutions/merge_two_lists.java
+++ Leetcode_Solutions/merge_two_lists.java
@@ -33,3 +33,28 @@ class Solution
 		return dummy.next;
 	}
 }
+
+
+// Recursion, https://www.youtube.com/watch?v=qckKEYP9bBA
+// Time: O(m+n) Space: O(m+n)
+
+class Solution 
+{
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) 
+	{
+		// Base Case
+		if (l1 == null) return l2;
+		if (l2 == null) return l1;
+		
+		if (l1.val < l2.val)
+		{
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} 
+		else
+		{
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
+		}
+	}
+}
