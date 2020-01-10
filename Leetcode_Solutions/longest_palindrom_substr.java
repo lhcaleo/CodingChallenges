9f0d5160a19dac9389f3905b2ce13d63a25d3794
hao <lihaocaleo@gmail.com>
2020-01-09 21:39:12 -0500

    solve by expand-around-center

--- Leetcode_Solutions/longest_palindrom_substr.java
+++ Leetcode_Solutions/longest_palindrom_substr.java
@@ -25,3 +25,34 @@ class Solution
 		return result;
 	}
 }
+
+
+// https://leetcode.com/problems/longest-palindromic-substring/
+// Expand Around Center
+// Time: O(n^2) Space: O(1)
+class Solution 
+{
+	String result = "";
+	public String longestPalindrome(String s) 
+	{
+		if (s.isEmpty() || s == null)
+			return s;
+		for (int i = 0; i < s.length(); i++)
+		{
+			expandAroundCenter(s, i, i);
+			expandAroundCenter(s, i, i+1);
+		}
+		return result;   
+	}
+	private void expandAroundCenter(String s, int left, int right)
+	{
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+		{
+			left--;
+			right++;
+		}
+		String current = s.substring(left + 1, right);
+		if (current.length() > result.length())
+			result = current;
+	}
+}
