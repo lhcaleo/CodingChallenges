3c31f67bafc111f8f96ff0ec4506acfe66c30983
hao <lihaocaleo@gmail.com>
2020-01-10 22:35:29 -0500

    update BFS iterative solution

--- Leetcode_Solutions/max_depth_binary_tree.java
+++ Leetcode_Solutions/max_depth_binary_tree.java
@@ -21,3 +21,35 @@ class Solution
 		return Math.max(maxDepthLeft, maxDepthRight) + 1;
 	}
 }
+
+/**
+ * Iterative approach BFS, record the level, when finish, get max depth
+ * Time: O(n) 
+ * Worst Space: O(n) completely not balanced binary tree eg. each node has only left child
+ * Best Space: O(log n) with balanced binary tree as height of tree is log n
+ */
+class Solution 
+{
+	public int maxDepth(TreeNode root) 
+	{
+		if (root == null) return 0;
+		
+		int max_depth = 0;
+		LinkedList<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		
+		while (!queue.isEmpty())
+		{
+			max_depth++;
+			int level_size = queue.size();
+			
+			for (int i = 0; i < level_size; i++)
+			{
+				TreeNode curr = queue.poll();
+				if (curr.left != null) queue.add(curr.left);
+				if (curr.right != null) queue.add(curr.right);
+			}
+		}
+		return max_depth;
+	}
+}
