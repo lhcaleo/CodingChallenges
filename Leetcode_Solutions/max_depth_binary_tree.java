f6de2ed13c02caca8dd6750c619ffe61549aa097
hao <lihaocaleo@gmail.com>
2020-01-13 15:59:42 -0500

    update 3 line best recursion

--- Leetcode_Solutions/max_depth_binary_tree.java
+++ Leetcode_Solutions/max_depth_binary_tree.java
@@ -53,3 +53,13 @@ class Solution
 		return max_depth;
 	}
 }
+// Best Recursion Solution
+
+class Solution 
+{
+	public int maxDepth(TreeNode root) 
+	{
+		if (root == null) return 0;
+		return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+	}
+}
