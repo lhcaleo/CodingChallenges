747d01367b5bcb97f248233f8f994046aded2460
hao <lihaocaleo@gmail.com>
2020-02-09 23:05:06 -0500

    solve by using priority queue

--- Leetcode_Solutions/KthLargestElement.java
+++ Leetcode_Solutions/KthLargestElement.java
@@ -1,3 +1,23 @@
+// priority queue with k elements
+// Time: O(n log k) Space: O(k)
+class Solution 
+{
+	public int findKthLargest(int[] nums, int k) 
+	{
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int num: nums)
+		{
+			pq.add(num);
+			// keep the size of queue at K
+			if (pq.size() > k)
+			{
+				pq.poll();
+			}
+		}
+		return pq.peek();
+	}
+}
+
 // quicksort
 class Solution 
 {
