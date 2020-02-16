8e930e60065dc055c220ca1f69912f837b51d648
hao <lihaocaleo@gmail.com>
2020-02-15 23:49:54 -0500

    Update sqrt(x)_bs.java

--- Leetcode_Solutions/sqrt(x)_bs.java
+++ Leetcode_Solutions/sqrt(x)_bs.java
@@ -1,3 +1,36 @@
+class Solution 
+{
+	public int mySqrt(int x) 
+	{
+		if (x == 0 || x == 1) 
+			return x;
+		
+		int start = 1;
+		int end = x;
+		int mid = 0;
+		int sqrt = 0;
+		
+		while (start <= end)
+		{
+			mid = start + (end - start) / 2;
+			sqrt = x/mid;
+			
+			if (sqrt == mid)
+			{
+				return mid;
+			} else if (mid < sqrt) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		
+		return end;
+	}
+}
+
+
+
 class Solution {
 	public int mySqrt(int x) {
 		if(x == 0 || x ==1) return x;
