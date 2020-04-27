b84cfb03fe1bcaddd4a530bf77afdfe4d15442ff
hao <lihaocaleo@gmail.com>
2020-04-27 00:19:07 -0400

    Create Max Consecutive Ones.java

---
+++ Leetcode_Solutions/Max Consecutive Ones.java
@@ -0,0 +1,23 @@
+// Time: O(n) Space: O(1)
+class Solution 
+{
+	public int findMaxConsecutiveOnes(int[] nums) 
+	{
+		int max = 0;
+		int temp = 0;
+		
+		for (int num: nums)
+		{
+			if (num == 1)
+			{
+				temp++;
+			} 
+			else if (num == 0) 
+			{
+				temp = 0;
+			}
+			max = Math.max(max, temp);
+		}
+		return max;
+	}
+}
